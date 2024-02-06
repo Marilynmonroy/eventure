ef0eda4e9c6831b593f16327092bd8a4a031202a
Marilynmonroy <artlyn.monroy@gmail.com>
2024-02-06 00:44:04 -0300

    .

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -9,28 +9,21 @@ export interface IEvent extends Document {
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  price?: string;
+  price: string;
   isFree: boolean;
   url?: string;
-  category: {
-    _id: string;
-    name: string;
-  };
-  organizer: {
-    _id: string;
-    firstname: string;
-    lastname: string;
-  };
+  category: { _id: string; name: string };
+  organizer: { _id: string; firstName: string; lastName: string };
 }
 
 const EventSchema = new Schema({
-  title: { type: String, requiered: true },
+  title: { type: String, required: true },
   description: { type: String },
   location: { type: String },
   createdAt: { type: Date, default: Date.now },
   imageUrl: { type: String, required: true },
-  startDataTime: { type: Date, default: Date.now },
-  endDataTime: { type: Date, default: Date.now },
+  startDateTime: { type: Date, default: Date.now },
+  endDateTime: { type: Date, default: Date.now },
   price: { type: String },
   isFree: { type: Boolean, default: false },
   url: { type: String },
@@ -38,6 +31,6 @@ const EventSchema = new Schema({
   organizer: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
-const Event = models.User || model("Event", EventSchema);
+const Event = models.Event || model("Event", EventSchema);
 
 export default Event;
